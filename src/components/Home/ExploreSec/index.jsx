ba21412fa5a1e97b88e4c6c381afd74ec70cfc35
moelshenawy <mohamedgk5@gmail.com>
2024-02-15 01:26:30 +0200

    ..

--- src/components/Home/ExploreSec/index.jsx
+++ src/components/Home/ExploreSec/index.jsx
@@ -1,125 +1,296 @@
-import React from 'react'
-import styles from './index.module.scss'
-import Image from 'next/image'
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
+// import React, { useState } from 'react'
+// import styles from './index.module.scss'
+// import Image from 'next/image'
+// import { Swiper, SwiperSlide } from 'swiper/react';
+// import 'swiper/css';
+
+// const ExploreSec = () => {
+
+//   const [hovered, setHovered] = useState(null);
+
+//   const onHoverStart = (index) => setHovered(index);
+//   const onHoverEnd = () => setHovered(null);
+
+//   // Define variants for Framer Motion animations
+//   const boxVariants = {
+//     hovered: { width: 480, height: 800, zIndex: 2 },
+//     unhovered: { width: 100, height: 100, zIndex: 0 },
+//   };
+
+//   const contentVariants = {
+//     show: { display: 'block', opacity: 1, transition: { delay: 0.2 } },
+//     hide: { display: 'none', opacity: 0 },
+//   };
+
+//   return (
+//     <section id='explore' className={styles.explore}>
+//       <div className="container">
+//         <div className={`${styles.sec_title}  sec_title`}>
+//           <h3>استكشف المدينة المنورة</h3>
+
+//           <div className={styles.icon_container}>
+//             <Image width={208.76} height={209.51} src='/assets/svgs/Safe_Icon.svg' />
+//           </div>
+//         </div>
+//         <div className={styles.boxes_container}>
+//           <Swiper
+//             breakpoints={{
+//               300: {
+//                 slidesPerView: 1,
+//                 spaceBetween: 0,
+//               },
+//               400: {
+//                 slidesPerView: 1,
+//                 spaceBetween: 0,
+//               },
+//               414: {
+//                 slidesPerView: 1,
+//                 spaceBetween: 0,
+//               },
+//               640: {
+//                 slidesPerView: 1,
+//                 spaceBetween: 0,
+//               },
+//               768: {
+//                 slidesPerView: 2,
+//                 spaceBetween: 0,
+//               },
+//               1024: {
+//                 slidesPerView: 3,
+//                 spaceBetween: 0,
+//               },
+//             }}
+//             dir={`rtl`}
+//             className={styles.swiper_container}
+//           >
+
+//             <SwiperSlide className={styles.swiper_slide_box}>
+//               <div className={styles.box}>
+//                 <div className={styles.background_image}>
+//                   <Image width={208.76} height={209.51} src='/assets/images/roads.png' />
+//                 </div>
+
+//                 <div className={styles.icon_container}>
+//                   <Image width={208.76} height={209.51} src='/assets/svgs/location_flag.svg' />
+//                 </div>
+
+//                 <div className={styles.title}>
+//                   <h5>دروب المدينة</h5>
+//                 </div>
+
+//                 <div className={styles.desc}>
+//                   <p>جولات معدة بعناية وفق اهتماماتك واحتياجاتك.</p>
+//                 </div>
+
+//               </div>
+//             </SwiperSlide>
+//             <SwiperSlide className={styles.swiper_slide_box}>
+
+//               <div className={styles.box}>
+//                 <div className={styles.background_image}>
+//                   <Image width={208.76} height={209.51} src='/assets/images/Bage_middle.png' />
+//                 </div>
+
+//                 <div className={styles.icon_container}>
+//                   <Image width={208.76} height={209.51} src='/assets/svgs/castle_icon_purble.svg' />
+//                 </div>
+
+//                 <div className={styles.title}>
+//                   <h5>معالم المدينة</h5>
+//                 </div>
+
+//                 <div className={styles.desc}>
+//                   <p>زر أشهر معالم المدينة التاريخية والحديثة</p>
+//                 </div>
+
+//               </div>
+//             </SwiperSlide>
+//             <SwiperSlide className={styles.swiper_slide_box}>
+
+//               <div className={styles.box}>
+//                 <div className={styles.background_image}>
+//                   <Image width={208.76} height={209.51} src='/assets/images/Bage_Left.png' />
+//                 </div>
+
+//                 <div className={styles.icon_container}>
+//                   <Image width={208.76} height={209.51} src='/assets/svgs/Vector_icon.svg' />
+//                 </div>
+
+//                 <div className={styles.title}>
+//                   <h5>مرافق المدينة </h5>
+//                 </div>
+
+//                 <div className={styles.desc}>
+//                   <p>استمتع بزيارة عدة مرافق متنوعة </p>
+//                 </div>
+
+//               </div>
+//             </SwiperSlide>
+//           </Swiper>
+
+
+
+
+
+//         </div>
+
+//       </div>
+//     </section>
+//   )
+// }
+
+// export default ExploreSec
+
+
+// sssssss
+
+
+import React, { useState } from 'react';
+import { motion } from 'framer-motion';
+import styles from './index.module.scss'; // Ensure this is the correct path to your SCSS file
+import Image from 'next/image';
 
 const ExploreSec = () => {
+  const [hoveredIndex, setHoveredIndex] = useState(-1);
+
+
+
+  const boxData = [
+    {
+      id: 1,
+      title: 'دروب المدينة',
+      description: 'جولات معدة بعناية وفق اهتماماتك واحتياجاتك.',
+      image: '/assets/images/roads.png',
+      icon: '/assets/svgs/location_flag.svg'
+    },
+    {
+      id: 2,
+      title: 'معالم المدينة',
+      description: 'زر أشهر معالم المدينة التاريخية والحديثة',
+      image: '/assets/images/Bage_middle.png',
+      icon: '/assets/svgs/castle_icon_purble.svg'
+    },
+    {
+      id: 3,
+      title: 'مرافق المدينة ',
+      description: 'استمتع بزيارة عدة مرافق متنوعة.',
+      image: '/assets/images/Bage_Left.png',
+      icon: '/assets/svgs/Vector_icon.svg'
+    },
+  ];
+
+  const boxVariants = {
+    normal: {
+      width: '500px',
+      height: '800px',
+      transition: { duration: 0.2 }
+    },
+    hovered: {
+      width: '1580px',
+      height: '800px',
+      transition: { duration: 0.2 }
+    },
+    smallBox: {
+      width: '200px',
+      height: '200px',
+      backgroundColor: 'red',
+      transition: { duration: 0.2 }
+    }
+  };
+
   return (
     <section id='explore' className={styles.explore}>
-      <div className="container">
-        <div className={`${styles.sec_title}  sec_title`}>
-          <h3>استكشف المدينة المنورة</h3>
+      <div className={styles.boxes_container}>
+        {boxData.map((box, index) => (
 
-          <div className={styles.icon_container}>
-            <Image width={208.76} height={209.51} src='/assets/svgs/Safe_Icon.svg' />
-          </div>
-        </div>
-        <div className={styles.boxes_container}>
-          <Swiper
-            breakpoints={{
-              300: {
-                slidesPerView: 1,
-                spaceBetween: 0,
-              },
-              400: {
-                slidesPerView: 1,
-                spaceBetween: 0,
-              },
-              414: {
-                slidesPerView: 1,
-                spaceBetween: 0,
-              },
-              640: {
-                slidesPerView: 1,
-                spaceBetween: 0,
-              },
-              768: {
-                slidesPerView: 2,
-                spaceBetween: 0,
-              },
-              1024: {
-                slidesPerView: 3,
-                spaceBetween: 0,
-              },
-            }}
-            dir={`rtl`}
-            className={styles.swiper_container}
-          >
 
-            <SwiperSlide className={styles.swiper_slide_box}>
-              <div className={styles.box}>
-                <div className={styles.background_image}>
-                  <Image width={208.76} height={209.51} src='/assets/images/roads.png' />
-                </div>
-
+          <>
+            {/* 
+            <motion.div
+              key={box.id}
+              className={styles.box}
+              onMouseEnter={() => setHoveredIndex(index)}
+              onMouseLeave={() => setHoveredIndex(null)}
+              animate={hoveredIndex === index ? 'hovered' : 'normal'}
+              variants={boxVariants}
+            >
+              {hoveredIndex === index ? (
+                <motion.div
+                  className={styles.innerBoxes}
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  exit={{ opacity: 0 }}
+                >
+                  {[...Array(3)].map((_, i) => (
+                    <motion.div key={i} variants={boxVariants.smallBox} />
+                  ))}
+                </motion.div>
+              ) : (
                 <div className={styles.icon_container}>
-                  <Image width={208.76} height={209.51} src='/assets/svgs/location_flag.svg' />
+                  <img src={box.icon} alt="Icon" />
                 </div>
+              )}
+            </motion.div> */}
 
-                <div className={styles.title}>
-                  <h5>دروب المدينة</h5>
-                </div>
 
-                <div className={styles.desc}>
-                  <p>جولات معدة بعناية وفق اهتماماتك واحتياجاتك.</p>
-                </div>
-
+            <motion.div
+              key={box.id}
+              className={styles.box}
+              onMouseEnter={() => setHoveredIndex(index)}
+              onMouseLeave={() => setHoveredIndex(null)}
+              animate={hoveredIndex === index ? 'hovered' : 'normal'}
+              variants={boxVariants}>
+              <div className={styles.background_image}>
+                <Image width={208.76} height={209.51} src={box.image} />
               </div>
-            </SwiperSlide>
-            <SwiperSlide className={styles.swiper_slide_box}>
 
-              <div className={styles.box}>
-                <div className={styles.background_image}>
-                  <Image width={208.76} height={209.51} src='/assets/images/Bage_middle.png' />
-                </div>
-
-                <div className={styles.icon_container}>
-                  <Image width={208.76} height={209.51} src='/assets/svgs/castle_icon_purble.svg' />
-                </div>
-
-                <div className={styles.title}>
-                  <h5>معالم المدينة</h5>
-                </div>
-
-                <div className={styles.desc}>
-                  <p>زر أشهر معالم المدينة التاريخية والحديثة</p>
-                </div>
-
+              <div className={styles.icon_container}>
+                <Image width={208.76} height={209.51} src={box.icon} />
               </div>
-            </SwiperSlide>
-            <SwiperSlide className={styles.swiper_slide_box}>
-
-              <div className={styles.box}>
-                <div className={styles.background_image}>
-                  <Image width={208.76} height={209.51} src='/assets/images/Bage_Left.png' />
-                </div>
-
-                <div className={styles.icon_container}>
-                  <Image width={208.76} height={209.51} src='/assets/svgs/Vector_icon.svg' />
-                </div>
-
-                <div className={styles.title}>
-                  <h5>مرافق المدينة</h5>
-                </div>
-
-                <div className={styles.desc}>
-                  <p>استمتع بزيارة عدة مرافق متنوعة</p>
-                </div>
-
+              {/* 
+              <div className={styles.title}>
+                <h5>{box.title}</h5>
               </div>
-            </SwiperSlide>
-          </Swiper>
+
+              <div className={styles.desc}>
+                <p>جولات معدة بعناية وفق اهتماماتك واحتياجاتك.</p>
+              </div> */}
 
 
+              {hoveredIndex === null || hoveredIndex === index ? (
+                <motion.div
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  exit={{ opacity: 0 }}
+                  transition={{ duration: 0.3 }}
+                  className={styles.inner_container}
+                >
+
+                  <div className={styles.text_container}>
+                    <div className={styles.title}>
+                      <h5>{box.title}</h5>
+                    </div>
+                    <div className={styles.desc}>
+                      <p>جولات معدة بعناية وفق اهتماماتك واحتياجاتك.</p>
+                    </div>
+                  </div>
+                </motion.div>
+              ) :
+                ''
+              }
 
 
+              {hoveredIndex === index && <h1></h1>}
 
-        </div>
+            </motion.div >
 
+
+          </>
+        ))}
       </div>
-    </section>
-  )
-}
+    </section >
 
-export default ExploreSec
+  );
+};
+
+export default ExploreSec;
--- src/components/Home/HeaderSection/index.jsx
+++ src/components/Home/HeaderSection/index.jsx
@@ -1,27 +1,142 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../../Navbar'
 import Image from 'next/image'
 import styles from './index.module.scss'
 import Mosque from '@/svgs/Mosque.svg'
 import { IoIosArrowBack } from "react-icons/io";
 import { useRouter } from 'next/router'
+import { AnimatePresence, motion } from 'framer-motion';
+import Link from 'next/link'
+
+
+const images2 =
+  [
+    { imgSrc: '/assets/bannerImgs/mosque.svg', shapeColor: 'linear-gradient(180deg, #D3C1BD 0%, rgba(211, 193, 189, 0) 129.34%)', bgColor: 'linear-gradient(271deg, #B7A697 0%, #377A8A 60.34%)' },
+    { imgSrc: '/assets/bannerImgs/bulding.svg', shapeColor: 'linear-gradient(180deg, rgba(56, 57,105, 0.4) 0%, rgba(211, 193, 189, 0) 129.34%)', bgColor: 'linear-gradient(86deg, #8E8EAE 60%, #d3c1bd00 88.34%)' },
+    { imgSrc: '/assets/bannerImgs/rock.svg', shapeColor: 'linear-gradient(180deg, rgba(56, 122, 138, 0.3) 0%, rgba(211, 193, 189, 0) 129.34%)', bgColor: 'linear-gradient(90deg, rgb(183, 166, 151) 58%, rgb(55, 122, 138) 93.34%)' },
+  ]
+
+
+
+
 
 const HeaderSection = () => {
   const route = useRouter()
+
+
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const currentImage = images2[currentIndex];
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((currentIndex) => (currentIndex + 1) % images2.length);
+    }, 5000); // Rotate images every 9 seconds
+    return () => clearInterval(interval);
+  }, []);
+
+  // Animation variants for framer-motion
+  const imageVariants = {
+    hidden: { opacity: 0.5, scale: 0.95 },
+    visible: { opacity: 1, scale: 1 },
+  };
+
+
   return (
     <>
       <header id={'header'} className={styles.header}
-        style={{
-          backgroundImage:
-            route.pathname === '/' ?
-              'url("/assets/images/Banner.png")'
-              :
-              'url("/assets/images/BannerPage1.png")'
-        }}>
+        style={{ background: currentImage.bgColor }}
+      >
         <Navbar />
-      </header>
 
-      {route.pathname === '/' &&
+
+        <div className={styles.top_cloud}>
+          <Image src={'/assets/bannerImgs/cloud2.png'} width={1440} height={413} />
+
+        </div>
+
+
+        <div className="container">
+
+          <div className={styles.banner_container} >
+
+
+
+            <div className={styles.right_side}>
+
+
+              <div className={styles.main_title}>
+                <h2>المدينة المنورة</h2>
+              </div>
+
+
+            </div>
+
+
+            <div className={`${styles.left_side}`} >
+              <motion.div
+                key={currentImage.imgSrc} // Key changes on image change, triggering re-render
+                initial="hidden"
+                animate="visible"
+                variants={imageVariants}
+                transition={{ duration: 1 }} // Smooth transition over 1 second
+
+
+                className={`${styles.main_img_container}`}>
+
+                <img src={currentImage.imgSrc} width={539} height={546.45} alt="Banner Image"
+                />
+
+                <div className={styles.birds}>
+                  <Image src={'/assets/bannerImgs/birds.svg'} width={128} height={82} />
+                </div>
+
+
+
+              </motion.div>
+
+
+              <div className={styles.shape} style={{ background: currentImage.shapeColor }} />
+
+
+            </div>
+          </div>
+
+        </div>
+
+
+
+
+
+        <motion.div
+          key={currentImage.imgSrc} // Key changes on image change, triggering re-render
+          initial="hidden"
+          animate="visible"
+          variants={imageVariants}
+          transition={{ duration: 1 }} // Smooth transition over 1 second
+
+          className={`${styles.blur_img}`}
+
+        >
+          <Image src={currentImage.imgSrc} width={1440} height={413} />
+        </motion.div>
+
+
+        <div className={styles.lines}>
+          <Image src={'/assets/bannerImgs/lines.svg'} width={8169.95} height={2105.82} />
+
+        </div>
+
+
+        <div className={styles.cloud}>
+          <Image src={'/assets/bannerImgs/cloud2.png'} width={1440} height={413} />
+        </div>
+
+
+      </header >
+
+      {
+        route.pathname === '/' &&
         <section id='hero' className={styles.hero}>
           <div className="container">
 
@@ -61,7 +176,7 @@ const HeaderSection = () => {
             <div className={styles.boxes_container2}>
 
 
-              <div className={styles.box}>
+              <Link href={'#virtual_guide'} className={styles.box}>
                 <div className={styles.background_image}>
                   <Image src={'/assets/images/card_bg.png'} width={
                     201} height={165} />
@@ -78,7 +193,7 @@ const HeaderSection = () => {
                   <Image width={124.45} height={178.6} src="/assets/images/Background_hands_mob.png" alt="" />
 
                 </div>
-              </div>
+              </Link>
 
 
               <div className={styles.box}>
